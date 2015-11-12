4c469e5b566df74f29d781e08890949a839af414
MilesHD <miles.dickinson92@gmail.com>
2015-11-12 18:26:53 -0500

    Refactor for meal management, add meal fixture

--- app.js
+++ app.js
@@ -4,47 +4,143 @@ angular.module('ionicApp', ['ionic'])
 
         $stateProvider
 
-            .state('news', {
+            .state('meals', {
                 abstract: true,
-                url: '/news',
-                templateUrl: 'news.html'
+                url: '/meals',
+                templateUrl: 'meals.html'
             })
 
-            .state('news.list', {
+            .state('meals.list', {
                 url: '',
-                templateUrl: 'news.list.html',
-                controller: 'NewsListController'
+                templateUrl: 'meals.list.html',
+                controller: 'MealsListController'
             })
 
-            .state('news.detail', {
+            .state('meals.detail', {
                 url: '/:id',
-                templateUrl: 'news.detail.html',
-                controller: 'NewsDetailController'
+                templateUrl: 'meals.detail.html',
+                controller: 'MealsDetailController'
             });
 
-        $urlRouterProvider.otherwise("/news");
+        $urlRouterProvider.otherwise("/meals");
 
     })
 
-    .controller('NewsListController', function ($scope, $location) {
+    .controller('MealsListController', function ($scope, $location) {
 
-        $scope.news = [
-            { id: 1, name: 'News 1' },
-            { id: 2, name: 'News 2' },
-            { id: 3, name: 'News 3' },
-            { id: 4, name: 'News 4' },
-            { id: 5, name: 'News 5' }
+        $scope.meals = [
+            {
+                id: 1,
+                name: 'Salmon Done Right',
+                ingredients: [
+                    {
+                        name: "Shredded Red Cabbage",
+                        quantity: "4 cups"
+                    },
+                    {
+                        name: "Mini Red Potatoes",
+                        quantity: "12"
+                    },
+                    {
+                        name: "Extra virgin olive oil",
+                        quantity: "2 tbsp"
+                    },
+                    {
+                        name: "Salt",
+                        quantity: "Preference"
+                    },
+                    {
+                        name: "Pepper",
+                        quantity: "Preference"
+                    },
+                    {
+                        name: "Lemon",
+                        quantity: "1"
+                    }
+                ],
+                preparation: [
+                    {
+                        step: 1,
+                        instruction: "Preheat oven to 400 Degrees"
+                    },
+                    {
+                        step: 2,
+                        instruction: "In a roasting pan, toss tabbage and potatoes with olive oil, season with salt and pepper"
+                    },
+                    {
+                        step: 3,
+                        instruction: "Roast for 25 minutes"
+                    },
+                    {
+                        step: 4,
+                        instruction: "Nestle salmon in center of pan, pushing cabbage and potatoes to edges. Season salmon with salt and pepper. Cut lemon in half and squeeze 1 half over dish."
+                    },
+                    {
+                        step: 5,
+                        instruction: "Roast for 15 minutes. Squeeze other lemon half over dish and serve."
+                    }
+                ]
+            }
         ];
 
-        $scope.onSelectNews = function(n) {
-            //$state.go('news.detail', { NewsId: n.id });
-            $location.url('/news/' + n.id);
+        $scope.onSelectMeal = function(n) {
+            $location.url('/meals/' + n.id);
         }
 
     })
 
-    .controller('NewsDetailController', function ($stateParams, $scope) {
+    .controller('MealsDetailController', function ($stateParams, $scope) {
 
-        $scope.news = { id: $stateParams.id, name: 'News ' + $stateParams.id };
-
+        $scope.meal = {
+                id: 1,
+                name: 'Salmon Done Right',
+                ingredients: [
+                    {
+                        name: "Shredded Red Cabbage",
+                        quantity: "4 cups"
+                    },
+                    {
+                        name: "Mini Red Potatoes",
+                        quantity: "12"
+                    },
+                    {
+                        name: "Extra virgin olive oil",
+                        quantity: "2 tbsp"
+                    },
+                    {
+                        name: "Salt",
+                        quantity: "Preference"
+                    },
+                    {
+                        name: "Pepper",
+                        quantity: "Preference"
+                    },
+                    {
+                        name: "Lemon",
+                        quantity: "1"
+                    }
+                ],
+                preparation: [
+                    {
+                        step: 1,
+                        instruction: "Preheat oven to 400 Degrees"
+                    },
+                    {
+                        step: 2,
+                        instruction: "In a roasting pan, toss tabbage and potatoes with olive oil, season with salt and pepper"
+                    },
+                    {
+                        step: 3,
+                        instruction: "Roast for 25 minutes"
+                    },
+                    {
+                        step: 4,
+                        instruction: "Nestle salmon in center of pan, pushing cabbage and potatoes to edges. Season salmon with salt and pepper. Cut lemon in half and squeeze 1 half over dish."
+                    },
+                    {
+                        step: 5,
+                        instruction: "Roast for 15 minutes. Squeeze other lemon half over dish and serve."
+                    }
+                ]
+            };
     });
